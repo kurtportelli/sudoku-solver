e0e4e37aa8b7a88d7e77bbcaffabd6f0b7fdae0c
Kurt Portelli <kurt.portelli@gig.com>
2023-02-17 11:39:50 +0100

    feat: validate the input grid

--- sudoku_solver/validators.py
+++ sudoku_solver/validators.py
@@ -18,4 +18,31 @@ def validate_input(input_grid: Any) -> list[list[int]]:
             if not isinstance(value, int):
                 raise InvalidInputValuesException
 
-    return [[0] * 9] * 9
+    for row_index in range(9):
+        for column_index in range(9):
+            value = input_grid[row_index][column_index]
+
+            if value == 0:
+                continue
+
+            row = input_grid[row_index]
+            if row.count(value) != 1:
+                raise InvalidInputValuesException
+
+            column = [row[column_index] for row in input_grid]
+            if column.count(value) != 1:
+                raise InvalidInputValuesException
+
+            box_row_start = (row_index // 3) * 3
+            box_column_start = (column_index // 3) * 3
+
+            box = []
+            for box_row_index in range(box_row_start, box_row_start + 3):
+                box.extend(
+                    input_grid[box_row_index][box_column_start : box_column_start + 3]
+                )
+
+            if box.count(value) != 1:
+                raise InvalidInputValuesException
+
+    return input_grid
--- tests/test_validators.py
+++ tests/test_validators.py
@@ -31,8 +31,42 @@ def test_validate_input_invalid_input_grid(input_grid: Any) -> None:
     ["input_grid"],
     [
         pytest.param([[""] * 9] * 9, id="not-integer-values"),
+        pytest.param([[1] * 9] * 9, id="duplicate-values-in-row"),
+        pytest.param(
+            [[1, 0, 0, 0, 0, 0, 0, 0, 0]] * 9, id="duplicate-values-in-column"
+        ),
+        pytest.param(
+            [
+                [1, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 1, 0, 0, 0, 0, 0, 0, 0],
+                [0] * 9,
+                [0] * 9,
+                [0] * 9,
+                [0] * 9,
+                [0] * 9,
+                [0] * 9,
+                [0] * 9,
+            ],
+            id="duplicate-values-in-box",
+        ),
     ],
 )
 def test_validate_input_invalid_input_values(input_grid: Any) -> None:
     with pytest.raises(InvalidInputValuesException):
         validate_input(input_grid)
+
+
+def test_validate_input_valid_input() -> None:
+    validate_input(
+        [
+            [1, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 2, 0, 0, 0, 0, 0, 0, 0],
+            [3, 0, 0, 4, 0, 0, 0, 0, 0],
+            [2, 0, 0, 0, 3, 0, 0, 0, 0],
+            [0] * 9,
+            [0] * 9,
+            [0] * 9,
+            [0] * 9,
+            [0] * 9,
+        ]
+    )
